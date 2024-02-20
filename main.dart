5f0784e496d60d8f8e5df3b0105a3cfb16cd22d3
Bala M. S <138640711+Ba223464@users.noreply.github.com>
2024-02-20 19:37:50 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(PortfolioApp());
+}
+
+class PortfolioApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Portfolio',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: PortfolioScreen(),
+    );
+  }
+}
+
+class PortfolioScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Portfolio'),
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // About Section
+            Text(
+              'About Me',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Bala M S\nComputer Science Engineering Student\nAge:20',
+              style: TextStyle(fontSize: 16),
+            ),
+            SizedBox(height: 20),
+
+            // Projects Section
+            Text(
+              'Projects',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            ProjectCard(
+              title: 'Project 1',
+              description: 'Description-Application for portfolio ',
+            ),
+            ProjectCard(
+              title: 'Project 2',
+              description: 'Description-Design works',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ProjectCard extends StatelessWidget {
+  final String title;
+  final String description;
+
+  ProjectCard({required this.title, required this.description});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 3,
+      margin: EdgeInsets.symmetric(vertical: 10),
+      child: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              title,
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Text(
+              description,
+              style: TextStyle(fontSize: 16),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
